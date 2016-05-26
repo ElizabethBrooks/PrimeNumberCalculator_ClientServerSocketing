9b55acfd9ebc9758c5bab64fef2b58009c0ed175
ElizabethBrooks <merimaat1@gmail.com>
2016-05-26 10:59:35 -0400

    Updated client server socket connections and random number generation

--- primeCalculator_client.c
+++ primeCalculator_client.c
@@ -14,39 +14,80 @@ Date Modified: May 26, 2016
 #include <netdb.h>
 #include <unistd.h>
 #include <errno.h>
+#include <math.h>
+//Method for efficiently determining if an input number is prime
+int primeCalculator(int numInput){
+   int i; //Initialize counter
+   for(i=2; i < sqrt(numInput); i++){ //Check values until the sqrt of number
+      if(numInput%i == 0){ //Check if number is divisible by i
+         return 0; //Number is not prime
+      } //End if
+   } //End for
+   return 1; //Number is prime
+} //End primeCalculator
 //The main method for a client application to send large prime numbers to a client application through TCP/IP socketing
 int main(int argc, char *argv[]){
-    int sfd = 0, count = 0; //Initialize variables for socket conection
+    int sfd=0, num=0, timeout=0; //Initialize variables for socket conection
     struct sockaddr_in servAdd; //Struct for socket
-    char rBuffer[1024]; //Input buffer char array
-    if(argc != 2){
-        printf("\n Usage: %s <ip of server> \n",argv[0]); //Print ip
-        return 1; //Exit program return
-    } //End if 
-    memset(rBuffer, '0',sizeof(rBuffer)); //Initialize input buffer
-    if((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){ //Create and verift socket connection to server
-        printf("\n Error : Could not create socket \n"); //Print error message if failed
+    const char equalsDel[2]="="; //Set delimiter for string tokenization
+    char rBuffer[1044], fileString[144], *token="ini", line[144]; //Input buffer char array
+    FILE *userFile=0; //Initialize input file object
+    memset(rBuffer, '0', sizeof(rBuffer)); //Initialize input buffer
+    memset(&servAdd, '0', sizeof(servAdd)); //Initialize socket struct
+    if(argc != 2){ //Verify input parameters
+        printf("\nUsage: %s filename\n",argv[0]); //Print filename
         return 1; //Exit program return
     } //End if
-    memset(&servAdd, '0', sizeof(servAdd)); //Initialize socket struct
-    servAdd.sin_family = AF_INET; //Set socket family/domain
-    servAdd.sin_port = htons(5000); //Set port for socket connection
-    if(inet_pton(AF_INET, argv[1], &servAdd.sin_addr)<=0){ //Attempt to set internet pton
-        printf("\n inet_pton error occured\n"); //Print error if internet pton failed
-        return 1; //Exit program return
+    userFile=fopen(argv[1], "rt"); //Open input file from command line
+    if(userFile){ //Verify the file exists
+        while(fgets(line, 144, userFile) != NULL){ //Parse input file by line, up to 144 characters
+            sscanf(line, "%s", &fileString); //Retrieve current file line
+            token=strtok(fileString, equalsDel); //Retrieve first token
+            while(token != NULL){ //Tokenize file string for input parameters
+                if(strcmp(token,"ip")){ //Check if ip
+                    token=strtok(NULL, equalsDel); //Retrieve next token
+                    localaddr.sin_addr.s_addr=inet_addr(token); //Set ip
+                }else if(strcmp(token,"port")){ //Check if port
+                    token=strtok(NULL, equalsDel); //Retrieve next token
+                    servAdd.sin_port=htons(token); //Set port
+                }else if(strcmp(token,"timeout")){ //Check if timeout
+                    token=strtok(NULL, equalsDel); //Retrieve next token
+                    sscanf(token, "%d", &timeout); //Convert string token to int
+                }else{ //Invalid input file string token
+                    printf("\nError: invalid input file... using default ip, port, and timeout values.\n"); //Print error message
+                    servAdd.sin_port=htons(4444); //Set default port
+                    servAdd.sin_addr.s_addr=inet_addr("192.168.1.100"); //Set default ip
+                } //End if, else if
+            } //End while
+        } //End while
+    }else{ //Return error if file does not exist
+        printf("File not found... using default ip, port, and timeout values.\n"); //Print error message
+        servAdd.sin_port=htons(4444); //Set default port
+        servAdd.sin_addr.s_addr=inet_addr("192.168.1.100"); //Set default ip
+    }//End else
+    fclose(userFile); //Close input file 
+    servAdd.sin_family=AF_INET; //Set socket family/domain   
+    if((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){ //Create and verift socket connection to server
+        printf("\nError: socket creation failure\n"); //Print error message if failed
+        return -1; //Exit program with error
     } //End if
     if(connect(sfd, (struct sockaddr *)&servAdd, sizeof(servAdd)) < 0){ //Attempt connection between client and server sockets
-       printf("\n Error : Connection Failed \n"); //Print error message if connection failed
-       return 1; //Exit program return
+       printf("\nError: connection failure\n"); //Print error message if connection failed
+       return -1; //Exit program with error
     } //End if
-    while ((count = read(sfd, rBuffer, sizeof(rBuffer)-1)) > 0){ //Retrieve prime number sent through server/client socket connection
-        rBuffer[count] = 0;
-        if(fputs(rBuffer, stdout) == EOF){ //Check if buffer is empty
-            printf("\n Error : fputs error\n"); //Print error message for input buffer
+    while((num=read(sfd, rBuffer, sizeof(rBuffer)-1)) > 0){ //Retrieve prime number sent through server/client socket connection
+        rBuffer[num]=0; //Clear output buffer
+        if(primeCalculator(num) == 1){ //Determine if input number is prime
+            printf("%d is a prime number!\n", num); //Print that the number is prime
+        }else{ //Input number is not prime
+            printf("%d is not a prime number!\n", num); //Print that the number is not prime
+        } //End if, else
+        if(fputs(rBuffer, stdout) == EOF){ //Display result of calcuation
+            printf("\nError : results display failure\n"); //Print error message for output buffer
         } //End if
     } //End while
-    if(count < 0){ //Check number sent is valid
-        printf("\n Read error \n"); //Print error message for invalid prime number
+    if(num < 0){ //Check number sent is valid
+        printf("\nError: read failure\n"); //Print error message for invalid prime number
     } //End if
     return 0; //End of program return
 } //End main
--- primeCalculator_server.c
+++ primeCalculator_server.c
@@ -13,24 +13,46 @@ Date modified: May 26, 2016
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
-//The main method for a server application to send large prime numbers to a client application through TCP/IP socketing
-int main(int argc, char *argv[]){
-    int lfd = 0, cfd = 0; //Initialize variables for socket
-    struct sockaddr_in servAdd; //Struct for socket
-    char sBuffer[1025]; //Output buffer char array
-    lfd = socket(AF_INET, SOCK_STREAM, 0); //Create a new unnamed socket and store its file descriptor
-    memset(&servAdd, '0', sizeof(servAdd)); //Initialize socket struct
+#include <limits.h>
+//Method for sending random numbers to clients using threads
+void send_random(int lfdInput){
+    int cfd=0; //Initialize variable for sending numbers to clients
+    char sBuffer[1044]; //Output buffer char array
     memset(sBuffer, '0', sizeof(sBuffer)); //Initialize output buffer
-    servAdd.sin_family = AF_INET; //Set family/domain
-    servAdd.sin_addr.s_addr = htonl(INADDR_ANY); //Set interface for server to listen on
-    servAdd.sin_port = htons(5000); //Set port to wait for client response on
-    bind(lfd, (struct sockaddr*)&servAdd, sizeof(servAdd)); //Bind the socket details to the created server struct, servers must bind with specific port
-    listen(lfd, 12); //Set max number of client connections for server
+    unsigned long randomNum=0, limit=ULONG_MAX; //Initialize variables for random number generation
+    while(limit > 0){ //Set random number while bits greater than 0 remain
+           randomNum=(randomNum*(INT_MAX+1))+rand(); //Increment randomNum
+           limit/=(INT_MAX+1); //Decrement limit
+    } //End while
+    snprintf(sendBuff, sizeof(sendBuff), "%d", randomNum); //Send random number to output buffer
     while(1){ //Wait for client responses
-        cfd = accept(lfd, (struct sockaddr*)NULL, NULL); //Once client is ready, retrieve the client socket file descriptor
-        //snprintf(sBuffer, sizeof(sBuffer), "%.24s\r\n", ctime(&ticks));
+        cfd=accept(lfdInput, (struct sockaddr*)NULL, NULL); //Once client is ready, retrieve the client socket file descriptor
         write(cfd, sBuffer, strlen(sBuffer)); //Use client descriptor to write back to client
         close(cfd); //Close the current client socket using its file descriptor
-        sleep(1);
+        sleep(1); //Pause client process
      } //End while
+} //End send_random
+//The main method for a server application to send large prime numbers to a client application through TCP/IP socketing
+int main(int argc, char *argv[]){
+    int lfd=0, threadNum=0, i=0; //Initialize variables for socket
+    struct sockaddr_in servAdd; //Struct for socket
+    lfd=socket(AF_INET, SOCK_STREAM, 0); //Create a new unnamed socket and store its file descriptor
+    memset(&servAdd, '0', sizeof(servAdd)); //Initialize socket struct
+    servAdd.sin_addr.s_addr=inet_addr("192.168.1.100"); //Set interface for server to listen on
+    servAdd.sin_port=htons(4444); //Set port to wait for client response on
+    servAdd.sin_family=AF_INET; //Set family/domain
+    bind(lfd, (struct sockaddr*)&servAdd, sizeof(servAdd)); //Bind the socket details to the created server struct, servers must bind with specific port
+    listen(lfd, 5); //Set max number of client connections for server, 5 is recommended max for most systems
+    pthread_t threadPool[5]; //Thread pool structure
+    for(threadNum=0; threadNum < 5; threadNum++){ //Loop and create threads
+        fflush(stdout); //Flush output buffer
+        int Error = pthread_create(&threadPool[threadNum],NULL,send_random,(void *)&lfd); //Create threads for searching neighborhoods
+        if (Error) { //Catch errors in creating threads
+            printf("Error creating threads... program exited."); //Print error message to the screen
+            return -1; //Exit program with error
+      } //End if
+    } //End for
+    for(i=0; i < 5; i++){ //Join client threads
+        pthread_join(threadPool[i],(void **)0);
+    } //End for
 } //End main
